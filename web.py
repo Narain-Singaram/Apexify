49711405067a6cbb3fd3f704e363b43de7d20039
Narain Singaram <narain.awesome@gmail.com>
2022-11-25 22:47:58 -0500

    creating tons of functions

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import functions as f
+import random as rand
 
 def crt_todo():
     todo = st.session_state["new_todo"]
@@ -7,24 +8,43 @@ def crt_todo():
     f.write_todos(todos)
     print(todo)
 
+def edit_todo:
+
+
+def repeated_code():
+    st.write("Created by: Narain Singaram")
+    st.title("Apexify | To-Do List App")
 
 todos = f.get_todos()
 
-st.write("Created by: Narain Singaram")
-st.title("Apexify | To-Do List App")
+tab1, tab2, tab3 = st.tabs(["Main", "Edit", "Owl"])
 
-st.text_input(label="Suggest a To-Do",
-              placeholder="What's on your mind?",
-              on_change=crt_todo,
-              key="new_todo")
+with tab1:
+    repeated_code()
+    st.text_input(label="Suggest a To-Do",
+                  placeholder="What's on your mind?",
+                  on_change=crt_todo,
+                  key="new_todo")
 
-
-for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
-    if checkbox:
-        todos.pop(index)
-        f.write_todos(todos)
-        del st.session_state[todo]
-        st.experimental_rerun()
+    for index, todo in enumerate(todos):
+        checkbox = st.checkbox(todo, key=todo)
+        if checkbox:
+            todos.pop(index)
+            f.write_todos(todos)
+            del st.session_state[todo]
+            st.experimental_rerun()
+        else:
+            pass
+    if st.button('Shuffle Order'):
+        rand.shuffle(todos)
     else:
         pass
+
+with tab2:
+    repeated_code()
+    st.text_input(label="Edit a To-Do",
+                  placeholder="Select the current position of the to-do to edit. Ex. 2",
+                  on_change=edit_todo,
+                  key="edit_todo")
+with tab3:
+    print("water")
