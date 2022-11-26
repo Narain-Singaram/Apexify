2aee7f970ee6b92a99833339ac733dbe99371ddf
Narain Singaram <narain.awesome@gmail.com>
2022-11-25 22:15:51 -0500

    Created input and checkboxes

--- web.py
+++ web.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import functions as f
 
-st.title("Apexify | To-Do List App")
+todos = f.get_todos()
+
 st.write("Created by: Narain Singaram")
+st.title("Apexify | To-Do List App")
+
+st.text_input(label="Suggest a To-Do", placeholder="What's on your mind?")
+
+for todo in todos:
+    st.checkbox(todo)
