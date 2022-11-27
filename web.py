a3290fec2d8a540a39ec267ddb2b16ed7c0f9e0a
Narain Singaram <narain.awesome@gmail.com>
2022-11-27 14:37:13 -0500

    if this dooesnt work i

--- web.py
+++ web.py
@@ -26,7 +26,7 @@ def edit_todo(index):
 
 def repeated_code():
     st.write("Created by: Narain Singaram")
-    st.title("Apexify | To-Do List App")
+    st.title("fdsafsafas | To-Do List App")
 
 todos = f.get_todos()
 tab1, tab2 = st.tabs(["Main", "Edit"])
